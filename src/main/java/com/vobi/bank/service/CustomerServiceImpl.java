7f8bbdf8d85e419e863ba190195772223a2dc43f
Diego Armando Gomez <diegomezusb@gmail.com>
2022-02-12 09:57:32 -0500

    Creacion service

---
+++ src/main/java/com/vobi/bank/service/CustomerServiceImpl.java
@@ -0,0 +1,76 @@
+package com.vobi.bank.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Validator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.vobi.bank.domain.Customer;
+import com.vobi.bank.repository.CustomerRepository;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+	
+	@Autowired
+	CustomerRepository customerRepository;
+	
+	@Autowired
+	Validator validator;
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Customer> findAll() {
+		return customerRepository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Customer> findById(Integer id) {
+		return customerRepository.findById(id);
+	}
+	
+	@Override
+	@Transactional(readOnly = true)
+	public Long count() {
+		return customerRepository.count();
+	}
+
+	@Override
+	@Transactional(readOnly = false,propagation = Propagation.REQUIRED)
+	public Customer save(Customer entity) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Customer update(Customer entity) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Customer entity) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void validate(Customer entity) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
