850c1454a0df69410a936c4418a35664bd9532d5
Diego Armando Gomez <diegomezusb@gmail.com>
2022-02-12 09:26:54 -0500

    Creacion metodo de debeBorrarUnCustomer

--- src/test/java/com/vobi/bank/repository/CustomerRepositoryIT.java
+++ src/test/java/com/vobi/bank/repository/CustomerRepositoryIT.java
@@ -2,6 +2,7 @@ package com.vobi.bank.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
@@ -14,8 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.vobi.bank.domain.Customer;
 import com.vobi.bank.domain.DocumentType;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
+@Slf4j
 class CustomerRepositoryIT {
 	
 	@Autowired
@@ -102,5 +106,24 @@ class CustomerRepositoryIT {
 		
 		assertFalse(customerOptional.isPresent(),"No pudo borrar el customer");
 	}
+	
+	@Test
+	@Order(5)
+	void debeConsultarTodosLosCustomers() {
+		//Arrange
+		List<Customer> customers=null;
+		
+		//Act
+		
+		customers=customerRepository.findAll();
+		
+		customers.forEach(customer->log.info(customer.getName()));		
+		
+		//Assert
+		
+		assertFalse(customers.isEmpty(),"No consulto Customers");	
+	}
+	
+	
 	
 }
