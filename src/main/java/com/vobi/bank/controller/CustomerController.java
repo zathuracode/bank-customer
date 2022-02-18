dea15993a1b3ac466fdb03dc046ec88c9a72c486
Diego Armando Gomez <diegomezusb@gmail.com>
2022-02-18 18:58:33 -0500

    Desarrollo de Customer Controller

--- src/main/java/com/vobi/bank/controller/CustomerController.java
+++ src/main/java/com/vobi/bank/controller/CustomerController.java
@@ -1,5 +1,25 @@
 package com.vobi.bank.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vobi.bank.domain.Customer;
+import com.vobi.bank.service.CustomerService;
+
+@RestController
+@RequestMapping("/api/v1/customers")
 public class CustomerController {
+	
+	@Autowired
+	CustomerService customerService;
+	
+	@GetMapping()
+	public List<Customer> findAll()throws Exception{
+		return null;
+	}
 
 }
