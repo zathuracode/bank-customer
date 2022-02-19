5e6c59943ac167606a6e82f12204bf51e1eec9f9
Diego Armando Gomez <diegomezusb@gmail.com>
2022-02-18 20:07:56 -0500

    Desarrollo de Customer Controller

--- src/main/java/com/vobi/bank/mapper/CustomerMapper.java
+++ src/main/java/com/vobi/bank/mapper/CustomerMapper.java
@@ -1,5 +1,7 @@
 package com.vobi.bank.mapper;
 
+import java.util.List;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -7,11 +9,17 @@ import com.vobi.bank.domain.Customer;
 import com.vobi.bank.dto.CustomerDTO;
 
 @Mapper
-public interface CustomerMapper {
-	
+public interface CustomerMapper {	
 	
 	@Mapping(source = "documentType.dotyId" , target ="dotyId" )
 	public CustomerDTO customerToCustomerDTO(Customer customer);
+	
+	@Mapping(target = "documentType.dotyId" , source ="dotyId" )
+	public Customer customerDTOtoCustomer(CustomerDTO customerDTO);
+	
+	public List<CustomerDTO> customerListToCustomerDTOList(List<Customer> customers);
+	
+	public List<Customer> customerDTOListToCustomerList(List<CustomerDTO> customerDTOs);
 
 }
  
