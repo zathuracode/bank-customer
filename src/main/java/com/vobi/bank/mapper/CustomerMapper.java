36c455cb3a953782441168a0350c267f2bbef038
Diego Armando Gomez <diegomezusb@gmail.com>
2022-02-18 19:59:58 -0500

    Desarrollo de Customer Controller

--- src/main/java/com/vobi/bank/mapper/CustomerMapper.java
+++ src/main/java/com/vobi/bank/mapper/CustomerMapper.java
@@ -1,5 +1,15 @@
 package com.vobi.bank.mapper;
 
+import org.mapstruct.Mapper;
+
+import com.vobi.bank.domain.Customer;
+import com.vobi.bank.dto.CustomerDTO;
+
+@Mapper
 public interface CustomerMapper {
+	
+	
+	public CustomerDTO customerToCustomerDTO(Customer customer);
 
 }
+ 
